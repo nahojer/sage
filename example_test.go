b9edcfb0aaadcd2c93a3c236a33c11e7eb28c0d6
Johan Rönkkö <johan.ronkko@zimpler.com>
2022-12-25 14:03:37 +0100

    Example.

---
+++ example_test.go
@@ -0,0 +1,41 @@
+package sage_test
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+
+	"github.com/nahojer/sage"
+)
+
+func Example() {
+	type Handler func(ctx context.Context, w http.ResponseWriter, r *http.Request)
+
+	routes := sage.NewRouteTrie[Handler]()
+
+	routes.Add(http.MethodGet, "/ping/:pong", func(ctx context.Context, w http.ResponseWriter, r *http.Request) {
+		pong := ctx.Value("pong").(string)
+		w.WriteHeader(200)
+		fmt.Fprint(w, pong)
+	})
+
+	req := httptest.NewRequest(http.MethodGet, "http://localhost/ping/"+url.PathEscape("It's-a me, Mario!"), nil)
+	w := httptest.NewRecorder()
+
+	h, params, found := routes.Lookup(req)
+	if !found {
+		panic("never reached")
+	}
+
+	ctx := context.WithValue(req.Context(), "pong", params["pong"])
+	h(ctx, w, req)
+
+	fmt.Printf("Status: %d\n", w.Code)
+	fmt.Printf("Body: %q\n", w.Body.String())
+
+	// Output:
+	// Status: 200
+	// Body: "It's-a me, Mario!"
+}
